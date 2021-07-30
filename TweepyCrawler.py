87f31a14a2e6174d9bb651519b3b91b3b14dcf1b
ronngCoD <ron.ng@chainofdemand.co>
2021-07-30 14:03:13 +0800

    TweepyCrawler Beginning

--- TweepyCrawler.py
+++ TweepyCrawler.py
@@ -1,1 +1,15 @@
+import tweepy
 
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
