608da4afe580edb8947d02ed098bd226d19bc2d3
ronngCoD <ron.ng@chainofdemand.co>
2021-08-03 11:55:38 +0800

    TwintCrawler start

--- TweepyCrawler.py
+++ TweepyCrawler.py
@@ -1,8 +1,9 @@
 import tweepy
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+auth = tweepy.OAuthHandler("JPTDYUTk5FFA22yDXUwWqNA4F", "r8ySz1orfQTNNmT3TP7UULxuRTF4KMWHFKXqYZ7XXxD2077H3F")
+auth.set_access_token("1420930922401042433-OiqKEN7MPbagVBVTb8bREvFCEesq3Y", "4Cm7LikMZ502ZBrCQvrPDAjz42ioFCfTpwXm8uLH"
+                                                                            "LsiOD")
 
 # Create API object
 api = tweepy.API(auth, wait_on_rate_limit=True,
--- TwintCrawler.py
+++ TwintCrawler.py
@@ -2,22 +2,19 @@ import twint
 import json
 
 def scrape(keyword, limit):
-    # configuration
-    config = twint.Config()
-    config.Search = keyword
-    config.Lang = "en"
-    config.Limit = limit
-
-    config.Store_object = True
-    # running search
-    twint.run.Search(config)
-    tweets = twint.output.tweets_list
-    print(tweets)
-    #with open('data/' + keyword + '.txt', 'w', encoding='utf-8') as f:
-
-       # f.write(json.dumps(tweets) + '\n')
+    with open('data/' + keyword + '.txt', 'w', encoding='utf-8') as f:
+        # configuration
+        config = twint.Config()
+        config.Search = keyword
+        config.Lang = "en"
+        config.Limit = limit
+        config.Store_object = True
+        tweets = twint.output.tweets_list
+        # running search
+        twint.run.Search(config)
+        f.write(tweets)
 
 if __name__ == "__main__":
-    scrape('#payup', 10)
-
+    searchWord = '#payup'
+    scrape(searchWord, limit=200)
 
