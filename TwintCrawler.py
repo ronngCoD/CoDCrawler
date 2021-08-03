9780e68b1a93b3bad030e3f9ec544cbdd0a55c32
ronngCoD <ron.ng@chainofdemand.co>
2021-08-03 10:04:49 +0800

    TwintCrawler start

--- TwintCrawler.py
+++ TwintCrawler.py
@@ -1,1 +1,23 @@
+import twint
+import json
+
+def scrape(keyword, limit):
+    # configuration
+    config = twint.Config()
+    config.Search = keyword
+    config.Lang = "en"
+    config.Limit = limit
+
+    config.Store_object = True
+    # running search
+    twint.run.Search(config)
+    tweets = twint.output.tweets_list
+    print(tweets)
+    #with open('data/' + keyword + '.txt', 'w', encoding='utf-8') as f:
+
+       # f.write(json.dumps(tweets) + '\n')
+
+if __name__ == "__main__":
+    scrape('#payup', 10)
+
 
