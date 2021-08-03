6618c5e96498fe98413ec7ef94f64d3e4305ae65
ronngCoD <ron.ng@chainofdemand.co>
2021-08-03 13:56:35 +0800

    TwintCrawler #payup

--- TwintCrawler.py
+++ TwintCrawler.py
@@ -2,19 +2,19 @@ import twint
 import json
 
 def scrape(keyword, limit):
-    with open('data/' + keyword + '.txt', 'w', encoding='utf-8') as f:
-        # configuration
-        config = twint.Config()
-        config.Search = keyword
-        config.Lang = "en"
-        config.Limit = limit
-        config.Store_object = True
-        tweets = twint.output.tweets_list
-        # running search
-        twint.run.Search(config)
-        f.write(tweets)
+    # configuration
+    config = twint.Config()
+    config.Search = keyword
+    config.Lang = "en"
+    config.Limit = limit
+    config.Store_json = True
+    config.Output = keyword + ".txt"
+    # running search
+    twint.run.Search(config)
 
 if __name__ == "__main__":
     searchWord = '#payup'
     scrape(searchWord, limit=200)
+
+
 
